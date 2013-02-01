db2943548c1a1853b7fc21cdc93900bb208c6f62
Arron Schaar <aschaar@gmail.com>
2013-02-01 14:38:45 -0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+## Install
+
+*Requirements*
+
+Node / NPM
+
+Bower (Package Manager)
+
+```
+npm install bower -g
+```
+
+Inside your project run:
+
+```
+bower install x-tag-growbox
+```
+
+This downloads the component and dependencies to ./components
+
+
+
+## About
+
+Growbox resizes when the inner content changes sizes.  It's perfect for content that changes size and you don't want to have to resize the parent element.
+
+## Usage
+
+```
+	<x-growbox id="growbox">
+		<h1>HI</h1>
+		<p>Some content</p>
+	</x-growbox>
+```
+
---
+++ src/slidebox.js
@@ -0,0 +1,96 @@
+
+(function(){
+
+  var transform = xtag.prefix.js + 'Transform';
+  function getState(el){
+    var selected = xtag.query(el, 'x-slides > x-slide[selected]')[0] || 0;
+    return [selected ? xtag.query(el, 'x-slides > x-slide').indexOf(selected) : selected, el.firstElementChild.children.length - 1];
+  }
+
+  function slide(el, index){
+    var slides = xtag.toArray(el.firstElementChild.children);
+    slides.forEach(function(slide){ slide.removeAttribute('selected'); });
+    slides[index || 0].setAttribute('selected', null);
+    el.firstElementChild.style[transform] = 'translate'+ (el.getAttribute('orientation') || 'x') + '(' + (index || 0) * (-100 / slides.length) + '%)';
+  }
+
+  function init(toSelected){    
+    var slides = this.firstElementChild;
+    if (!slides || !slides.children.length || slides.tagName.toLowerCase() != 'x-slides') return;
+    
+    var children = xtag.toArray(slides.children),
+      size = 100 / (children.length || 1),
+      orient = this.getAttribute('orientation') || 'x',
+      style = orient == 'x' ? ['width', 'height'] : ['height', 'width'];
+    
+    slides.style[style[1]] =  '100%';
+    slides.style[style[0]] = children.length * 100 + '%';
+    slides.style[transform] = 'translate' + orient + '(0%)';
+    children.forEach(function(slide){       
+      slide.style[style[0]] = size + '%';
+      slide.style[style[1]] = '100%';
+    });    
+    
+    if (toSelected) {
+      var selected = slides.querySelector('[selected]');
+      if (selected) slide(this, children.indexOf(selected) || 0);
+    }
+  }
+
+  xtag.register('x-slidebox', {
+    events:{
+      'transitionend': function(e){
+        if (e.target == this) xtag.fireEvent(this, 'slideend');
+      },
+      'elementupgrade': function(e){
+        if (e.target == this){
+          init();
+        }
+      }
+    },
+    accessors:{
+      orientation:{
+        get: function(){
+          return this.getAttribute('orientation');
+        },
+        set: function(value){
+          this.setAttribute('orientation', value.toLowerCase());
+          init.call(this, true);
+        }
+      }
+    },
+    methods: {
+      slideTo: function(index){
+        slide(this, index);
+      },
+      slideNext: function(){
+        var shift = getState(this);
+          shift[0]++;
+        slide(this, shift[0] > shift[1] ? 0 : shift[0]);
+      },
+      slidePrevious: function(){
+        var shift = getState(this);
+          shift[0]--;
+        slide(this, shift[0] < 0 ? shift[1] : shift[0]);
+      }
+    }
+  });
+  
+  xtag.register('x-slide', {
+    lifecycle:{
+      inserted: function(){
+        var ancestor = this.parentNode.parentNode;
+        if (ancestor.tagName.toLowerCase() == 'x-slidebox') init.call(ancestor, true);
+      }
+    },
+    events:{
+      'elementupgrade': function(e){
+        if (e.target == this){
+          var ancestor = this.parentNode.parentNode;
+          if (ancestor.tagName.toLowerCase() == 'x-slidebox') init.call(ancestor, true);
+        }
+      }
+    }
+  });
+  
+})();
