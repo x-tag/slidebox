eb4ccbb31504dadff74dde3f6ba7a1659e44136e
Arron Schaar <aschaar@gmail.com>
2013-02-11 13:57:57 -0800

    added package.json and gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    
+    concat: {
+      options: {
+        separator: '\n',
+      },
+      'x-tag-js': {
+        src: [
+          'components/document.register/src/document.register.js', 
+          'components/x-tag-core/src/core.js',
+        ],
+        dest: 'demo/x-tag-components.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');  
+  grunt.registerTask('build', ['concat:x-tag-js']);
+
+
+};
